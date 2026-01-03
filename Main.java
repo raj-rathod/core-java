a928354162d40d37b0f44ab3760280d586ab4c23
Rajesh <rajesh@Rajeshs-MacBook-Air.local>
2026-01-04 00:13:56 +0530

    DSA hashing

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
+import numbers.NumberOperations;
 
 public class Main {
     public static void main(String[] arg) {
-   
+      NumberOperations nbObj = new NumberOperations();
+      System.out.println(nbObj.isEven(5));
 
     }
 }
