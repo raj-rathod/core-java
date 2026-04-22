98eebcf68520ca7e62af703927448a4d6c50c189
Rajesh <rajesh@Rajeshs-MacBook-Air.local>
2026-04-23 03:22:18 +0530

    feat:<Collection practice>

--- Main.java
+++ Main.java
@@ -1,23 +1,14 @@
-import dsa.LinkedListPractice;
-import dsa.ListNode;
-import dsa.RecursionPractice;
+import collections.CollectionPractice;
 
 public class Main {
     public static void main(String[] arg) {
-      //RecursionPractice obj = new RecursionPractice();
-      //System.out.println("Result: "+ obj.sampleFunc(3));
-      LinkedListPractice obj = new LinkedListPractice();
-      int[] arr = new int[]{1,2,3,4};
-      ListNode head = obj.createLinkedList(arr);
-      obj.printLinkedList(head);
-     // obj.reverseLinkedList(head);
-      //ListNode middleNode = obj.middleNode(head);
-      //obj.printLinkedList(middleNode);
-      //System.out.println("Middle node of the List is: "+ middleNode.value);
-      obj.createCycle(head, 2);
-      //System.out.println("Is this linkedlist has a cycle: "+ obj.isCycle(head));
-      ListNode cycleNode = obj.detectCycleNode(head);
-      System.out.println("Cyclic Node: "+ cycleNode.value);
+        CollectionPractice obj = new CollectionPractice();
+        int[] arr = {1,2,2,3,1,4,2};
+        //obj.frequencyRelated(arr);
+        //obj.removeDuplicates();
+        //obj.findIntersectionOfTwoList();
+        String[] strs = new String[]{"eat","tea","tan","ate","nat","bat"};
+        obj.findGroupOfAnagram(strs);
     }
 
 
